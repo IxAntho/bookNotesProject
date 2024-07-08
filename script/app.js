4fb20c5965020480528d11593095ebe5eb993e96
IxAntho <isabar735@icloud.com>
2024-07-08 11:20:27 -0400

    fixBug/app.js serve public static files

--- script/app.js
+++ script/app.js
@@ -2,6 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the current file path and directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // dotenv.config();
 // const password = process.env.PASSWORD;
@@ -9,13 +15,46 @@ import dotenv from "dotenv";
 const app = express();
 const port = 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "U-postgres",
-  password: `${password}`,
-  port: 5432,
+// const db = new pg.Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "U-postgres",
+//   password: `${password}`,
+//   port: 5432,
+// });
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up the correct path for static files
+const publicPath = path.join(__dirname, "..", "public");
+app.use("/public", express.static(publicPath));
+
+// Debugging middleware
+app.use((req, res, next) => {
+  console.log("Requested URL:", req.url);
+  console.log(
+    "File path:",
+    path.join(publicPath, req.url.replace("/public", ""))
+  );
+  next();
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.get("/", async (req, res) => {
+  await res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server Running on http://localhost:${port}`);
+});
+
+// // Close the database connection when the process is exiting
+// process.on("exit", () => {
+//   db.end();
+// });
+
+// // Close the database connection when the server is stopping
+// process.on("SIGINT", () => {
+//   db.end();
+//   process.exit();
+// });
